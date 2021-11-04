755151032deca67be17c0bb498a508c11dd9e4f0
alexwdevlin@gmail.com <P)rkupine9gi>
2021-11-04 09:27:31 -0700

    fixed style

--- main.py
+++ main.py
@@ -8,18 +8,19 @@ def clicked():
     status = Label(text="Stitching...")
     status.grid(column=2, row=btn_row)
     status.update()
-    chipName = id_entry.get()
-    filePath = path_txt.get() + "/"
+    chip_name = id_entry.get()
+    file_path = path_txt.get() + "/"
     x_images = int(x_size_entry.get())
     y_images = int(y_size_entry.get())
     picture_format = pic_formats.get()
     index_of_first_image = int(first_image_entry.get())
-    generateCollage(chipName, filePath, x_images, y_images, picture_format, index_of_first_image)
+    generate_collage(chip_name, file_path, x_images, y_images, picture_format, index_of_first_image)
     status["text"] = "Done!"
 
-def generateCollage(chipName, filePath, x_images, y_images, picture_format, index_of_first_image):
-    inspection_images = [Image.open(f) for f in fileLocations(x_images, y_images, filePath, index_of_first_image)]
-    collage = generateBlankImage(inspection_images[0], x_images, y_images)
+
+def generate_collage(chip_name, file_path, x_images, y_images, picture_format, index_of_first_image):
+    inspection_images = [Image.open(f) for f in file_locations(x_images, y_images, file_path, index_of_first_image)]
+    collage = generate_blankImage(inspection_images[0], x_images, y_images)
 
     x_position = 0
     y_position = 0
@@ -35,15 +36,15 @@ def generateCollage(chipName, filePath, x_images, y_images, picture_format, inde
             x_position += x_step
             y_position += pic.size[1]
 
-    collage.save(filePath + chipName + "." + picture_format)
+    collage.save(file_path + chip_name + "." + picture_format)
 
 
-def fileLocations(x_images, y_images, filePath, index_of_first_image):
+def file_locations(x_images, y_images, file_path, index_of_first_image):
     image_name_list = []
     number_of_images = x_images * y_images
 
     for i in range(number_of_images):
-        image_id = filePath
+        image_id = file_path
 
         # add leading zeroes to the file name that were assigned by the image captures software
         for j in range(3 - int(math.log(i + index_of_first_image) / math.log(10))):
@@ -53,9 +54,9 @@ def fileLocations(x_images, y_images, filePath, index_of_first_image):
     return image_name_list
 
 
-def generateBlankImage(firstImage, x_images, y_images):
+def generate_blankImage(first_image, x_images, y_images):
     # create a blank image with the proper dimensions
-    width, height = firstImage.size
+    width, height = first_image.size
     total_width = width * x_images
     total_height = height * y_images
     return Image.new('RGB', (total_width, total_height))
